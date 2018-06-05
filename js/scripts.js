a1e852e3aeab82328346f8eca7b505fd384fd8a2
Abel Trotter and Matt Smith <abel@>
2018-06-05 10:37:04 -0700

    Add reset btn, adjust business logic, display results

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 function countUpTo(countTo, countBy) {
-  console.log(countTo);
-  console.log(countBy);
-  var numberStorage = [];
   if ((numberCompare(countTo, countBy) === true) || (noZero(countTo, countBy) === true) || (noBlanks(countTo, countBy) === true) || (notNum(countTo, countBy) === true)) {
     alert("try again");
   } else {
-    for(var i = 0; i <= countTo; i += countBy) {
-      numberStorage.push(i);
-      console.log(numberStorage);
+    for(var i = countBy; i <= countTo; i += countBy) {
+      displayResults(i);
     }
   }
 }
@@ -36,14 +32,22 @@ function notNum(countTo, countBy) {
   }
 }
 
+function displayResults(number) {
+  $("#output").append("<li>" + number + "</li>");
+}
+
 $(document).ready(function() {
   $("#submit").click(function(e) {
     e.preventDefault();
 
     var countTo = parseInt($("#countTo").val());
     var countBy = parseInt($("#countBy").val());
-    console.log(countTo);
-    console.log(countBy);
     countUpTo(countTo, countBy);
   });
+
+  $("#reset").click(function(e) {
+    e.preventDefault();
+
+    location.reload();
+  })
 });
