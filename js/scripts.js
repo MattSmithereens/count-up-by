7b415570e8bb1f208f2ce81d5b53bcec38fcdd71
Abel Trotter and Matt Smith <matt@>
2018-06-05 10:26:57 -0700

    determine value of NaN in code

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,8 @@ function countUpTo(countTo, countBy) {
   console.log(countTo);
   console.log(countBy);
   var numberStorage = [];
-  if ((numberCompare() === true) || (noZero() === true)  ) {
-    alert("Count To should be lower than Count By");
-  }
-  if (countTo.length = 0) {
-    alert("You left something blank");
-  }
-  if (countBy.length = 0) {
-    alert("You left something blank");
+  if ((numberCompare(countTo, countBy) === true) || (noZero(countTo, countBy) === true) || (noBlanks(countTo, countBy) === true) || (notNum(countTo, countBy) === true)) {
+    alert("try again");
   } else {
     for(var i = 0; i <= countTo; i += countBy) {
       numberStorage.push(i);
@@ -18,13 +12,13 @@ function countUpTo(countTo, countBy) {
   }
 }
 
-function numberCompare(countBy, countTo) {
+function numberCompare(countTo, countBy) {
   if (countTo <= countBy) {
     return true;
   }
 }
 
-function noZero(countBy, countTo) {
+function noZero(countTo, countBy) {
   if ((countTo <= 0) || (countBy <= 0)) {
     return true;
   }
@@ -33,7 +27,13 @@ function noZero(countBy, countTo) {
 function noBlanks(countTo, countBy) {
   if ((countTo.length === 0) || (countBy.length === 0)) {
     return true;
-  }  
+  }
+}
+
+function notNum(countTo, countBy) {
+  if ((isNaN(countTo)) || (isNaN(countBy))) {
+    return true;
+  }
 }
 
 $(document).ready(function() {
@@ -42,6 +42,8 @@ $(document).ready(function() {
 
     var countTo = parseInt($("#countTo").val());
     var countBy = parseInt($("#countBy").val());
+    console.log(countTo);
+    console.log(countBy);
     countUpTo(countTo, countBy);
   });
 });
