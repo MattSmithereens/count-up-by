5b4e5afa4eac9bd21b0be9085a5624a0582b8a84
Abel Trotter and Matt Smith <matt@>
2018-06-05 09:59:05 -0700

    add conditions, business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,38 @@ function countUpTo(countTo, countBy) {
   console.log(countTo);
   console.log(countBy);
   var numberStorage = [];
-  for(var i = 0; i <= countTo; i += countBy) {
-    numberStorage.push(i);
-    console.log(numberStorage);
+  if ((numberCompare() === true) || (noZero() === true)  ) {
+    alert("Count To should be lower than Count By");
   }
+  if (countTo.length = 0) {
+    alert("You left something blank");
+  }
+  if (countBy.length = 0) {
+    alert("You left something blank");
+  } else {
+    for(var i = 0; i <= countTo; i += countBy) {
+      numberStorage.push(i);
+      console.log(numberStorage);
+    }
+  }
+}
+
+function numberCompare(countBy, countTo) {
+  if (countTo <= countBy) {
+    return true;
+  }
+}
+
+function noZero(countBy, countTo) {
+  if ((countTo <= 0) || (countBy <= 0)) {
+    return true;
+  }
+}
+
+function noBlanks(countTo, countBy) {
+  if ((countTo.length === 0) || (countBy.length === 0)) {
+    return true;
+  }  
 }
 
 $(document).ready(function() {
